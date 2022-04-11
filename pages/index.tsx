b5f37b7002bc7b16526652b44e4b26f62b8f0a3e
csdear <csdear138@gmail.com>
2022-04-11 11:29:00 -0500

    hunter Grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -16,10 +16,11 @@ export default function Home() {
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <h2 className={styles.description}>
+          Note our stock nextJS vercel uses a grid that we can later expound
+          upon...
+          {/* <code className={styles.code}>pages/index.js</code> */}
+        </h2>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
@@ -50,6 +51,8 @@ export default function Home() {
             </p>
           </a>
         </div>
+
+        <h2 className={styles.description}>But Moving on With GR HUNTER</h2>
       </main>
 
       <footer className={styles.footer}>
@@ -58,12 +61,12 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
     </div>
-  )
+  );
 }
